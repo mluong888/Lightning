75babada645f065924766fc6aefbea0c733dc399
unknown <maluong@697-334-Theoden.sf.k12.internal>
2015-09-02 11:37:28 -0700

    added ball and counter

--- Lightning.pde
+++ Lightning.pde
@@ -1,20 +1,23 @@
-int dRandom = (int)(Math.random()*301);
+int dRandom = (int)(Math.random()*601);
 int startX = 0;
 int startY = dRandom;
 int endX = 0;
 int endY = dRandom;
+int click = 0;
 
 void setup()
 {
-  size(300,300);
+  size(600,600);
   strokeWeight(3);
   background(0,0,0);
+  //noLoop();
   
 }
 void draw()
 {
+
 	stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
-	while(endX<300)
+	while(endX<600)
 	{
 		endX=startX+(int)(Math.random()*10);
 		endY=startY+(int)(Math.random()*20)-9;
@@ -22,19 +25,30 @@ void draw()
 	startX=endX;
 	startY=endY;
 	dRandom = dRandom+(int)(Math.random()*10)-5;
-	if(dRandom<0||dRandom>300)
+	if(dRandom<0||dRandom>600)
 	{
-		dRandom=150;
+		dRandom=(int)(Math.random()*600);
 	}
 }
+
+fill(255,0,0);
+ellipse(500,500,20,20);
+noStroke();
+fill(0,0,0);
+rect(0,0,1000,40);
+fill(255,0,0);
+text("# of clicks:" + click,30,30);
 }
 
 void mousePressed()
 {
+	//redraw();
+	click=click+1;
 	startX=0;
 	startY=dRandom;
 	endX=0;
 	endY=dRandom;
+	
 
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,43 +14,57 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int dRandom = (int)(Math.random()*301);
+int dRandom = (int)(Math.random()*601);
 int startX = 0;
 int startY = dRandom;
 int endX = 0;
 int endY = dRandom;
+int click = 0;
 
 public void setup()
 {
-  size(300,300);
+  size(600,600);
   strokeWeight(3);
   background(0,0,0);
+  //noLoop();
   
 }
 public void draw()
 {
+
 	stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
-	while(endX<300)
+	while(endX<600)
 	{
 		endX=startX+(int)(Math.random()*10);
 		endY=startY+(int)(Math.random()*20)-9;
 		line(startX,startY,endX,endY);
 	startX=endX;
 	startY=endY;
-	dRandom = dRandom+(int)(Math.random()*10)-2;
-	if(dRandom<0||dRandom>300)
+	dRandom = dRandom+(int)(Math.random()*10)-5;
+	if(dRandom<0||dRandom>600)
 	{
-		dRandom=150;
+		dRandom=(int)(Math.random()*600);
 	}
 }
+
+fill(255,0,0);
+ellipse(500,500,20,20);
+noStroke();
+fill(0,0,0);
+rect(0,0,1000,40);
+fill(255,0,0);
+text("# of clicks:" + click,30,30);
 }
 
 public void mousePressed()
 {
+	//redraw();
+	click=click+1;
 	startX=0;
 	startY=dRandom;
 	endX=0;
 	endY=dRandom;
+	
 
 }
 
